dacfae81e613ee4699dd00ffe68cc9836b930588
PedroMateus26 <pedromateusneto@gmail.com>
2020-09-22 08:44:05 -0300

    Subtotal & Total methods

--- src/main/java/com/estudante/projetoSpring/entities/OrderItem.java
+++ src/main/java/com/estudante/projetoSpring/entities/OrderItem.java
@@ -64,4 +64,35 @@ public class OrderItem implements Serializable {
 	public void setOrder(Product product) {
 		id.setProduct(product);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	public Double getSubTotal() {
+		return price*quantity;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 }
